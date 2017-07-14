dc2ee54e28c292561457c936a5b20c034efa8475
HappySeal <cafer15165@gmail.com>
2017-07-14 15:22:08 +0300

    V 0.0.3

--- hello.py
+++ hello.py
@@ -5,11 +5,8 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-def temp():
+def mws():
     Hum,Temp = Adafruit_DHT.read_retry(11,4)
-    return render_template('mws.html', temp="Temp")
-def hum():
-    Hum,Temp = Adafruit_DHT.read_retry(11,4)
-    return render_template('mws.html', hum="Hum")
+    return render_template('mws.html', temp=Temp,hum=Hum)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
