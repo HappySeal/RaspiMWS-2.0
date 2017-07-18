f353a3a6164866c01249b8f5faed87422a4e501b
HappySeal <cafer15165@gmail.com>
2017-07-18 20:00:36 +0300

    V 0.0.6

--- mws.py
+++ mws.py
@@ -1,12 +1,34 @@
 from flask import Flask
 from flask import render_template
 import Adafruit_DHT
+import time
 app = Flask(__name__)
 app.debug = True
 
+timeNow = time.time()
+Hum,Temp = Adafruit_DHT.read_retry(11,4)
+oldHum,oldTemp = Hum,Temp
+
+HumId,TempId = "stable","stable"
 @app.route("/")
 def mws():
-    Hum,Temp = Adafruit_DHT.read_retry(11,4)
-    return render_template('mws.html', temp=Temp,hum=Hum)
+    global timeNow,oldHum,oldTemp,Hum,Temp,HumId,TempId
+    if((time.time()-timeNow)>=600):
+        oldHum,oldTemp = Hum,Temp
+        Hum,Temp = Adafruit_DHT.read_retry(11,4)
+        timeNow=time.time()
+    if(oldTemp < Temp):
+        TempId = "up"
+    if(oldTemp > Temp):
+        TempId ="down"
+    if(oldHum < Hum):
+        HumId = "up"
+    if(oldHum > Hum):
+        HumId = "down"
+    if(oldTemp == Temp):
+        TempId = "stable"
+    if(oldHum == Hum):
+        HumId = "stable"
+    return render_template('mws.html', temp=Temp,hum=Hum,idHum=HumId,idTemp=TempId)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
