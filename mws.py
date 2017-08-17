5d69bf875c0a48a268d9bfdc345fd0ee5fab220c
HappySeal <cafer15165@gmail.com>
2017-08-17 13:51:00 +0300

    V 0.0.8

--- mws.py
+++ mws.py
@@ -1,21 +1,33 @@
 from flask import Flask
 from flask import render_template
 import Adafruit_DHT
-import time
+import Adafruit_BMP.BMP085 as BMP
+import time,sqlite3
 app = Flask(__name__)
 app.debug = True
 
 timeNow = time.time()
 Hum,Temp = Adafruit_DHT.read_retry(11,4)
 oldHum,oldTemp = Hum,Temp
-
+con = sqlite3.connect("airDB.db")
+cursor = con.cursor()
+def table():
+    cursor.execute("CREATE TABLE IF NOT EXISTS air (year TEXT,mon TEXT,day TEXT,pre INT,hum INT,tem INT)")
+table()
 HumId,TempId = "stable","stable"
 @app.route("/")
 def mws():
     global timeNow,oldHum,oldTemp,Hum,Temp,HumId,TempId
     if((time.time()-timeNow)>=600):
         oldHum,oldTemp = Hum,Temp
+        sensor = BMP180.BMP085()
+        Press = sensor.read_pressure()
         Hum,Temp = Adafruit_DHT.read_retry(11,4)
+        day = time.strftime("%d")
+        month = time.strftime("%m")
+        year = time.strftime("%y%")
+        cursor.execute("INSERT INTO air (year,mon,day,pre,hum,tem) VALUES (?,?,?,?,?,?)",(year,month,day,Press,Hum,Temp))
+        con.commit()
         timeNow=time.time()
     if(oldTemp < Temp):
         TempId = "up"
