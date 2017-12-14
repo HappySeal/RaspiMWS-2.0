41f89a714493e70d109bd1a91bd3139384d8ce99
HappySeal <cafer15165@gmail.com>
2017-12-14 22:09:08 +0300

    V1.0.6

--- mws.py
+++ mws.py
@@ -1,37 +1,37 @@
-##Oncelikle ihtiyacımız olan kutuphanelerimizi ekliyoruz
+##Oncelikle ihtiyacimiz olan kutuphanelerimizi ekliyoruz
 from flask import Flask
 from flask import render_template
 ##Flask kutuphanesini ekletince projemize ek olarak iki dosya ekleniyor(Static , Templates)
-##Templates de web de gorunecek olan html kodumuz , Static dosyamız da web sayfamızda kullanacagımız fotograflar ve grafik oluşturmak için kullanacagımız javascript kutuphanelerimiz
+##Templates de web de gorunecek olan html kodumuz , Static dosyamiz da web sayfamizda kullanacagimiz fotograflar ve grafik olusturmak icin kullanacagimiz javascript kutuphanelerimiz
 import Adafruit_DHT
 import Adafruit_BMP.BMP085 as BMP
 ##DTH11 ve BMP180 sensörlerimizin kutuphanelerini ekliyoruz
 import time,sqlite3,dateStr
-## Zamanı bulmal için kullanacagımız time , veri tabanı için kullandıgımız sqlite3 , bulundugumuz tarihi bir yazıya donuşturecek kendi yaptıgım dateStr modullerini ekledik
+## Zamani bulmak icin kullanacagimiz time , veri tabani icin kullandigimiz sqlite3 , bulundugumuz tarihi bir yaziya donusturecek kendi yaptigim dateStr modullerini ekledik
 app = Flask(__name__)
 app.debug = True
-#Flaskdan uygulamamızı kuruyoruz
+#Flaskdan uygulamamizi kuruyoruz
 grafikTarih = []
 grafikNem = []
 grafikSicaklik = []
 grafikBasinc=[]
-#Grafige dokecegimiz Tarih , Nem , Sıcaklık ve Basinc listelerini tanımladık
+#Grafige dokecegimiz Tarih , Nem , Sicaklik ve Basinc listelerini tanimladik
 def dakika():
     return int(time.strftime("%H"))+60*(int(time.strftime("%M")))
-## Anlık dakika degerini tanımlayacak fonksiyonu tanımlıyoruz
+## Anlik dakika degerini tanimlayacak fonksiyonu tanimliyoruz
 Sondk=dakika()
-##Ve 1 kere kayıt alıyoruz
+##Ve 1 kere kayit aliyoruz
 Nem,Sicaklik = Adafruit_DHT.read_retry(11,4)
-##DHT 11 Nem ve sıcaklık sensorunden gelen verileri çekiyoruz
+##DHT 11 Nem ve sicaklik sensorunden gelen verileri cekiyoruz
 con = sqlite3.connect("havaVT.db",check_same_thread=False)
-##Veritabanımız ile baglantı kuruyoruz
+##Veritabanimiz ile baglanti kuruyoruz
 cursor = con.cursor()
-##Degişiklik yapmamızı saglayacak imleç(cursor) degişkenimizi tanımlıyoruz
+##Degisiklik yapmamizi saglayacak imlec(cursor) degiskenimizi tanimliyoruz
 def table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS hava (saatTXT TEXT,tarihTXT TEXT,basinc INT,nem INT,sicaklik INT)")
-##Tablomuzu ve bunun bolumlerini tanımlıyoruz.
+    cursor.execute("CREATE TABLE iF NOT EXiSTS hava (saatTXT TEXT,tarihTXT TEXT,basinc iNT,nem iNT,sicaklik iNT)")
+##Tablomuzu ve bunun bolumlerini tanimliyoruz.
 table()
-## Ve tablomuzu oluşturuyoruz
+## Ve tablomuzu olusturuyoruz
 def cikisListesi():
     global grafikTarih,grafikSicaklik,grafikNem,grafikBasinc,con,cursor
     grafikTarih = []
@@ -46,23 +46,23 @@ def cikisListesi():
         grafikNem.append((entryAll[i])[3])
         grafikSicaklik.append((entryAll[i])[4])
         grafikBasinc.append((entryAll[i])[2])
-##Grafigimizde yer alacak degerleri veri tabanından çekerek degişkenlerimize ekliyoruz
+##Grafigimizde yer alacak degerleri veri tabanindan cekerek degiskenlerimize ekliyoruz
 cikisListesi()
 #print(grafikTarih,"\n",grafikNem,"\n",grafikSicaklik,"\n",grafikBasinc,"\n")
 @app.route("/")
-##Bu noktadan sonraki fonksiyonlarımız templates içinde bulunan aynı adlı html dosyamıza etki edeceldir
+##Bu noktadan sonraki fonksiyonlarimiz templates icinde bulunan ayni adli html dosyamiza etki edeceldir
 def mws():
     global Nem,Sicaklik,grafikNem,grafikTarih,grafikSicaklik,grafikBasinc,con,cursor,Sondk
     tahmin=""
     sensor = BMP.BMP085()
-    ##BMP180 basınç sensoru kuruyoruz
+    ##BMP180 basinc sensoru kuruyoruz
     Nem,Sicaklik = Adafruit_DHT.read_retry(11,4)
     ##DHT11 sensorunden gelen verileri okuyoruz
     Basinc = int(sensor.read_pressure()/100)
     ##BMP 180 sensorunden gelen verileri okuyoruz
     if(dakika()-Sondk >=1):
-        ##Eger son kayitdan 10 dk geçmiş ise
-        cursor.execute("INSERT INTO hava (saatTXT TEXT,tarihTXT TEXT,basinc INT,nem INT,sicaklik INT) VALUES (?,?,?,?,?)".format(dateStr.hourStr(),dateStr.export(),Basinc,Nem,Sicaklik))
+        ##Eger son kayitdan 10 dk gecmis ise
+        cursor.execute("iNSERT iNTO hava (saatTXT TEXT,tarihTXT TEXT,basinc iNT,nem iNT,sicaklik iNT) VALUES (?,?,?,?,?)".format(dateStr.hourStr(),dateStr.export(),Basinc,Nem,Sicaklik))
         con.commit()
         ##Verileri Veri tabanina kaydetiyoruz
         Sondk = dakika()
