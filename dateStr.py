bff12e0fe778ccfc0b4a034c41c966271e85f6af
HappySeal <cafer15165@gmail.com>
2017-12-15 15:07:27 +0300

    V1.1.15

--- dateStr.py
+++
@@ -1,5 +0,0 @@
-import time
-def export(dayBefore = 0):
-	return str(int(time.strftime("%d"))-dayBefore)+"/"+time.strftime("%m")+"/"+time.strftime("%Y")
-def hourStr():
-	return str(time.strftime("%H")+":"+time.strftime("%M"))
